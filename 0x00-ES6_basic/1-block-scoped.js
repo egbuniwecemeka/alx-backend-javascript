64763aa3fad4484c2292af9cd2df552a8f361961
egbuniwecemeka <egbuniwecemeka.com>
2024-10-01 14:37:24 +0100

    var hoisting fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task;
+  let task2;
+  
+  task = false
+  task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
