c4f53c4ebc33dc46cafdd0f1470fd29d0cfa0571
egbuniwecemeka <egbuniwecemeka.com>
2024-10-03 00:21:29 +0100

    No need to create empty objects before adding in properties

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -3,25 +3,14 @@ function getCurrentYear() {
   return date.getFullYear();
 }
 
-<<<<<<< HEAD
-export default function getBudgetForCUrrentYear(income, gpd, capita) {
+export default function getBudgetForCUrrentYear(income, gdp, capita) {
   const year = getCurrentYear();
 
   const budget = {
     [`income-${year}`]: income,
-    [`gpd-${year}`]: gpd,
-    [`capita-${year}`]: capita,
-  };
-=======
-export default function getBudgetForCUrrentYear(income, gdp, capita) {
-    const year = getCurrentYear();
-
-    const budget = {
-        [`income-${year}`]: income,
-        [`gdp-${year}`]: gdp,
-        [`capita-${year}`]: capita
-    }
->>>>>>> c011a7fd10eda72cd5588cce7c5b1c9f177a59de
+    [`gdp-${year}`]: gdp,
+    [`capita-${year}`]: capita
+  }
 
   return budget;
 }
