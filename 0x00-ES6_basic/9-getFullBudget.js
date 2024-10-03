35818db6abd77a8fd58116d0924c6e11f8ef3a6b
egbuniwecemeka <egbuniwecemeka.com>
2024-10-03 02:10:16 +0100

    eslint fix

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -11,7 +11,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
         getIncomeInEuros(income) {
             return `${income} euros`;
         },
-    }
+    };
 
     return fullBudget;
 }
