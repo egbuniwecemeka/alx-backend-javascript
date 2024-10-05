59d05cf4279d6918134c20c77ce060e3d3b43607
egbuniwecemeka <egbuniwecemeka.com>
2024-10-05 12:57:06 +0100

    reject Error object added

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,14 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // async action goes here
+    return new Promise((resolve, reject) => {
+        //async action goes here
 
-    // Boolean check for function check on async operation's
-    // success or failure
-    if (true) {
-      resolve('API call was successful');
-    } else {
-      reject('API call was unsuccessful');
-    }
-  });
+        // Boolean check for function check on async operation's
+        // success or failure
+        if (true) {
+            resolve('API call was successful');
+        }
+        else {
+            reject(Error('API call was unsuccessful'));
+        }
+    });
 }
