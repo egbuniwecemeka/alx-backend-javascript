a22e4e1a49bcb9392c511356953e0c93692d9fc6
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-10-05 15:56:11 +0000

    eslint fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,14 @@
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        const obj = {
-            status: 200,
-            body: 'Success',
-        };
+  return new Promise((resolve, reject) => {
+    const obj = {
+      status: 200,
+      body: 'Success',
+    };
 
-        if (success) {
-            resolve(obj);
-        } else {
-            reject(Error('The fake API is not working currently'));
-        }
-    });
+    if (success) {
+      resolve(obj);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
 }
