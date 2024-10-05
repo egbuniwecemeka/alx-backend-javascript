6f463fbfebc48abc277c3d92a3b935aa05216c2d
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-10-05 12:15:41 +0000

    eslint fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,17 +1,16 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        //async action should go here
+  return new Promise((resolve, reject) => {
+    // async action should go here
 
-        //dynamic variable for success or failure
-        const succesOrFailure = Math.random() >= 0.5;
+    // dynamic variable for success or failure
+    const succesOrFailure = Math.random() >= 0.5;
 
-        // Boolean check for function check on async operation's
-        // success or failure
-        if (succesOrFailure) {
-            resolve('API call was successful');
-        }
-        else {
-            reject(Error('API call was unsuccessful'));
-        }
-    });
+    // Boolean check for function check on async operation's
+    // success or failure
+    if (succesOrFailure) {
+      resolve('API call was successful');
+    } else {
+      reject(Error('API call was unsuccessful'));
+    }
+  });
 }
