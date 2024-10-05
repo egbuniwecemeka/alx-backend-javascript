26dae0a9bd55398444c366b709784b606e0c5e8d
egbuniwecemeka <egbuniwecemeka.com>
2024-10-05 12:39:16 +0100

    return a Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,14 @@
 export default function getResponseFromAPI() {
-    return new Promise();
+    return new Promise((resolve, reject) => {
+        //async action goes here
+
+        // Boolean check for function check on async operation's
+        // success or failure
+        if (true) {
+            resolve('API call was successful');
+        }
+        else {
+            reject('API call was unsuccessful');
+        }
+    });
 }
