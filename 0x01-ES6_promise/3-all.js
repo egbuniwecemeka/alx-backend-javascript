e4e295718f5fdb092615b1529ec9a8d1fcd2838f
egbuniwecemeka <egbuniwecemeka.com>
2024-10-06 10:50:56 +0100

    Handle multiple successful promises edit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
-export function handleProfileSignup() {
+export default function handleProfileSignup() {
     Promise.all([uploadPhoto(), createUser()])
     .then(([photoResult, userResult]) => {
         console.log(photoResult.body, userResult.firstName, userResult.lastName);
