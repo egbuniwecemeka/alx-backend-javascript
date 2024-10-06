83c970c292902921e95c29095dc12ee20e903fc0
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-10-06 10:23:42 +0000

    eslint fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()])
+  Promise.all([uploadPhoto(), createUser()])
     .then(([photoResult, userResult]) => {
-        console.log(photoResult.body, userResult.firstName, userResult.lastName);
+      console.log(photoResult.body, userResult.firstName, userResult.lastName);
     }).catch(() => {
-        console.log(Error('Signup system offline'));
+      console.log(Error('Signup system offline'));
     });
 }
