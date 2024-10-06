7c5946df3e740bdb53120d1047599f05115eda08
egbuniwecemeka <egbuniwecemeka.com>
2024-10-06 11:44:14 +0100

    file edit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export function handleProfileSignup() {
-<<<<<<< HEAD
-  Promise.all([uploadPhoto(), createUser()])
-=======
     return Promise.all([uploadPhoto(), createUser()])
->>>>>>> 003570f145533e0b8ee5be5200a2146e2d00f82a
     .then(([photoResult, userResult]) => {
       console.log(photoResult.body, userResult.firstName, userResult.lastName);
     }).catch(() => {
