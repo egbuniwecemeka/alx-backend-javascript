f3dfda7efa65e57915d3b1d3c4875e65b8e4edf0
egbuniwecemeka <egbuniwecemeka.com>
2024-10-06 22:48:22 +0100

    Reject the promises

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,7 +1,7 @@
 export default function uploadPhoto(filename) {
     return new Promise((resolve, reject) => {
         if (filename) {
-            reject(Error(`Error: ${filename} cannot be processed`));
+            reject(Error(`${filename} cannot be processed`));
         } else {
             resolve(`File ${filename} upload successful`);
         }
