102c42d70eb6e7937513bbaef92cd9184abcb658
egbuniwecemeka <egbuniwecemeka.com>
2024-10-08 11:19:06 +0100

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,13 @@ import uploadPhoto from "./5-photo-reject";
 export default function handleProfileSignup(firstName, lastName, fileName) {
     const user = signUpUser(firstName, lastName);
     const photo = uploadPhoto(fileName);
-return Promise.allSettled([user, photo]).then((result) => {
-    return result.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-});
+    return Promise.allSettled([user, photo])
+        .then((results) => {
+            return results.map((result) => {
+                return {
+                    status: result.status,
+                    value: result.status === 'fulfilled' ? result.value : result.reason,
+                };
+            });
+        });
 }
