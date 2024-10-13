994d583cb377a050bf917cd774f85814a103a600
egbuniwecemeka <egbuniwecemeka.com>
2024-10-13 16:39:08 +0100

    updates

--- 0x02-ES6_classes/1-main.js
+++
@@ -1,5 +0,0 @@
-import ClassRoom1 from "./0-class";
-
-const classmates = new ClassRoom1(20)
-
-console.log(classmates._maxStudentsSize)
--- 0x02-ES6_classes/study/0-rect_main.js
+++
@@ -1,6 +0,0 @@
-import Shape from "./0-basic_rect_class";
-
-const shape = new Shape(5, 10)
-
-console.log(shape.getArea())
-console.log([...shape.getSides()])
