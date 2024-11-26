f4e97fc10b03733a14d25c921d02957b9ab57589
egbuniwecemeka <egbuniwecemeka.com>
2024-11-26 07:15:44 +0100

    simple server

--- 0x05-Node_JS_basic/study/server.js
+++ 0x05-Node_JS_basic/study/server.js
@@ -1,9 +1,9 @@
-const startServer = require('http')
+const createServer = require('http')
 
 const host = '127.0.0.1'
 const port = 3000
 
-const server = startServer.createServer((req, res) => {
+const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World!');
