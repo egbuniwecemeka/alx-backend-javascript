83032f279b557a397f454ecaac781ac7b9ec3d42
egbuniwecemeka <egbuniwecemeka.com>
2024-11-26 20:06:01 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -11,5 +11,5 @@ process.stdin.on('data', (input) => {
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
