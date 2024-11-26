b22058922701b4d652e058ad364647b34cdbc5dc
egbuniwecemeka <egbuniwecemeka.com>
2024-11-26 19:26:19 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,8 +3,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Ensure stdin is in flowing mode and encoding is set.
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
+//process.stdin.resume();
+//process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (input) => {
     const name = input.trim();
