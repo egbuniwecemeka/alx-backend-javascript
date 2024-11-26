8a09ab6e82f77228bb3e711b7fe46ebe71d73a30
egbuniwecemeka <egbuniwecemeka.com>
2024-11-26 22:14:30 +0100

    Create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const app = require('http');
+
+const host = '127.0.0.1';
+const port = 1245;
+
+const server = app.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+});
+
+server.listen((host, port), () => {
+    console.log(`The server is runnting at ${host}:${port}/`);
+});
+
+module.exports = app
