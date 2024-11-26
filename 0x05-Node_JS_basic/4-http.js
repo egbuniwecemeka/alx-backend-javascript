5939ee369eeac4c8a5288b0327155e210511ba04
egbuniwecemeka <egbuniwecemeka.com>
2024-11-26 22:54:49 +0100

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -9,7 +9,7 @@ const app = http.createServer((req, res) => {
 
 // Start listening on port 1245
 app.listen(1245, () => {
-  console.log(`The server is runnting at 1245`);
+  console.log('The server is runnting at 1245');
 });
 
 module.exports = app;
