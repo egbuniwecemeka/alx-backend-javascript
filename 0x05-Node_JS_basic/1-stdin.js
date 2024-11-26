709be55223783ad64672c98554eb98108b7ec31c
egbuniwecemeka <egbuniwecemeka.com>
2024-11-26 19:47:56 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 // Ensure stdin is in flowing mode and encoding is set.
 process.stdin.resume();
