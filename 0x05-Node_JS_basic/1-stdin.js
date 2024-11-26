8db4f62a5efa6efd35a0ec929af840d8664358ad
egbuniwecemeka <egbuniwecemeka.com>
2024-11-26 13:50:21 +0100

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (input) => {
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}\n`);
+  }
+})
+
+process.stdin.on('end', () => {
+    console.log('This important software is now closing');
+})
