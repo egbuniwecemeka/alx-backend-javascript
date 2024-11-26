a952113900ed2c187a5ddb4298cd1ef02d6bb848
egbuniwecemeka <egbuniwecemeka.com>
2024-11-26 19:46:07 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,7 +8,7 @@ process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (input) => {
     const name = input.trim();
-    process.stdout.write(`Your name is: ${name}\n`);
+    console.log(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
