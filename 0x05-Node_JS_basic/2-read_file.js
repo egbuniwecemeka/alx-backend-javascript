8be839dd959fe588bce007e8ffc45c4487778249
egbuniwecemeka <egbuniwecemeka.com>
2024-11-27 07:36:59 +0100

    Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+  // Read the file synchronously
+  const data = fs.readFileSync(path, 'utf-8');
+
+  // SPLIT DATA INTO ROWS
+  const rows = data.trim().split('\n');
+
+  // Process the header and student data rows
+  const headers = rows.split(',');
+  const studentsNum = rows.slice(1);
+
+  // Group students by column
+  const fieldIndex = headers.indexOf('field');
+  const studentByField = {};
+
+  studentsNum.forEach((row) => {
+    const column = row.split(',');
+    const field = column[fieldIndex]
+
+    if (!studentByField[field]) {
+      studentByField = []
+    }
+    studentByField[field].push(column);
+  });
+
+  // Formatting output
+  console.log(`Number of students: ${studentsNum.length}`);
+  for(const [field, students] of Object.entries(studentByField)) {
+    console.log(`Number of students in ${field}: ${students.length}`);
+  }
+
+  } catch (error) {
+    console.log('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
