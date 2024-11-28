aa1c376a8ecbe76d6349236cae35f7ead30a1970
egbuniwecemeka <egbuniwecemeka.com>
2024-11-29 00:30:33 +0100

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+function countStudents(filePath) {
+  fs.readFileSync(filePath, 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+}
