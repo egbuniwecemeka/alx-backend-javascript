ee08fc0932a382da786801250cf362d26e561597
egbuniwecemeka <egbuniwecemeka.com>
2024-11-28 13:48:11 +0100

    Reading a file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,9 +4,15 @@ const fs = require('fs');
 
 function countStudents(filePath) {
 try {
+  // Read data from file
   const data = fs.readFileSync(filePath, 'utf8');
 
+  // Split data into rows seperated by a newline
   const rows = data.split('\n').filter((row) => row.trim() !== "");
+
+  const header = rows[0].slice(1);
+  console.log(header)
+
 
   console.log(`Number of students: ${rows.length}`);
 
