fef599bf6c0c3d52a0ace88da0725ef4ce7cef5d
egbuniwecemeka <egbuniwecemeka.com>
2024-11-28 15:59:14 +0100

    Reading a file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,32 +3,32 @@
 const fs = require('fs');
 
 function countStudents(filePath) {
-try {
-  // Read data from file
-  const data = fs.readFileSync(filePath, 'utf8');
+  try {
+    // Read data from file
+    const data = fs.readFileSync(filePath, 'utf8');
 
-  // Split data into rows seperated by a newline
-  const rows = data.split('\n').filter((row) => row.trim() !== "");
+    // Split data into rows seperated by a newline
+    const rows = data.split('\n').filter((row) => row.trim() !== "");
 
-  // Extract header and student data (number)
-  const header = rows[0].split(',');
-  const studentRows = rows.slice(1);
+    // Extract header and student data (number)
+    const header = rows[0].split(',');
+    const studentRows = rows.slice(1);
 
-  // Group students by field of study
-  const fields = {};
+    // Group students by field of study
+    const fields = {};
 
-  studentRows.forEach((row) => {
-    const studentData = row.split(',');
-    const field = studentData[studentData.length - 1]
-    const name = studentData[0];
+    studentRows.forEach((row) => {
+      const studentData = row.split(',');
+      const field = studentData[studentData.length - 1]
+      const name = studentData[0];
 
-    if (!fields[field]) {
-      fields[field] = []
-    }
-    fields[field].push(name);
+      if (!fields[field]) {
+        fields[field] = []
+      }
+      fields[field].push(name);
 
   });
-  
+
   console.log(`Number of students: ${studentRows.length}`);    
 
   // Formatting students per field
