d7c1fee13944d0c3f6d137302ac43f5017bd243c
egbuniwecemeka <egbuniwecemeka.com>
2024-11-28 14:57:01 +0100

    Reading a file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -10,11 +10,10 @@ try {
   // Split data into rows seperated by a newline
   const rows = data.split('\n').filter((row) => row.trim() !== "");
 
-  const header = rows[0].slice(0);
-
+  const header = rows[0].split(',');
   const studentNum = rows.slice(1);
   console.log(`Number of students: ${studentNum.length}`);
-
+  console.log(header);
 
 } catch (error) {
   console.error('Cannot load the database')
