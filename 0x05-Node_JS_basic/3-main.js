77ecea5e5f8b477377fdac7d6f00c9299a5fe20e
egbuniwecemeka <egbuniwecemeka.com>
2024-11-29 00:47:33 +0100

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-main.js
+++ 0x05-Node_JS_basic/3-main.js
@@ -2,7 +2,7 @@ const countStudents = require('./3-read_file_async');
 
 countStudents('database.csv')
     .then(() => {
-        console.log(Done)
+        console.log('Done!');
     }).catch((error) => {
         console.log(error);
     });
