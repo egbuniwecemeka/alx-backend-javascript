d4797a22376d9fbe7c3a24a302b1c0b2d73f869a
egbuniwecemeka <egbuniwecemeka.com>
2024-11-28 10:28:41 +0100

    Reading a file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,49 +1,14 @@
+#!/usr/bin/node
+
 const fs = require('fs');
 
-function countStudents(path) {
-  try {
-  // Read the file synchronously
-  const data = fs.readFileSync(path, 'utf-8');
+// set path to the file
+const filePath = './database.csv';
 
-  // split data into rows and extract headers
-  const rows = data.trim().split('\n');
-  const headers = rows[0].split(','); // Extract the first row as headers
-  const studentsNum = rows.slice(1);  // Exclude the header row
+try {
+  const data = fs.readFileSync(filePath, 'utf8');
 
-  // Check if data is present
-  if (studentsNum.length === 0) {
-    console.log('No student data found');
-    return;
-  }
-
-  // Find the index of the 'field' column
-  const fieldIndex = headers.indexOf('field');
-  if (fieldIndex === -1) {
-    throw new Error ("The field column is missing in the file.");
-  }
-
-  // Group students by field
-  const studentByField = {};
-
-  studentsNum.forEach((row) => {
-    const column = row.split(',');
-    const field = column[fieldIndex]
-
-    if (!studentByField[field]) {
-      studentByField = [];
-    }
-    studentByField[field].push(column);
-  });
-
-  // Formatting output
-  console.log(`Number of students: ${studentsNum.length}`);
-  for(const [field, students] of Object.entries(studentByField)) {
-    console.log(`Number of students in ${field}: ${students.length}`);
-  }
-
-  } catch (error) {
-    console.log('Cannot load the database');
-  }
+  console.log(data)
+} catch (error) {
+  console.error('Cannot load the database')
 }
-
-module.exports = countStudents;
