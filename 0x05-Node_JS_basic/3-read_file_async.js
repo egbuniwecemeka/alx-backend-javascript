5796e5eef62a4debb00d02a241ce792a809e2dcf
egbuniwecemeka <egbuniwecemeka.com>
2024-11-29 01:04:57 +0100

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,10 +3,15 @@
 const fs = require('fs');
 
 function countStudents(filePath) {
-  fs.readFileSync(filePath, 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (data, error) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+    });
   });
+
 }
 
 module.exports = countStudents;
