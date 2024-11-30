d05c737397ce1dbbfe92ec0189ab5cce665b6608
egbuniwecemeka <egbuniwecemeka.com>
2024-11-30 23:56:06 +0100

    Combining descriptions

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,23 @@
+function calculateNumber(type, a, b) {
+    const opsType = ['SUM', 'SUBTRACT', 'DIVIDE'];
+
+  if (!opsType.includes(type)) {
+    throw new Error('Does not include operation type');
+  }
+
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b)
+
+  if (type === opsType[0]) {
+    return roundedA + roundedB;
+  } else if (type === opsType[1]) {
+    return roundedA - roundedB;
+  } else if (type === opsType) {
+    if (roundedB === 0) {
+        throw new Error('Error');
+    }
+    return roundedA / roundedB;
+  }
+}
+
+module.exports = calculateNumber;
