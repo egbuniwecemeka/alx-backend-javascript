3fdb75880d3f887f4cb982c14d76c7de8760e98c
egbuniwecemeka <egbuniwecemeka.com>
2024-11-30 13:44:54 +0100

    tests folder & file

--- 0x06-unittests_in_js/0-calcul_test.js
+++ 0x06-unittests_in_js/0-calcul_test.js
@@ -3,6 +3,6 @@ const calculateNumber = require('./0-calcul');
 
 describe('Function', function () {
     it('should return the sum of two numbers', function () {
-        assert.equal(calculateNumber(2, 3), 5)  // Testing with 2 + 3 = 5
+        assert.strictEqual(calculateNumber(2, 3), 5)  // Testing with 2 + 3 = 5
     })
 })
