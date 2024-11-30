d0ff9bd405fb95e17db59c14dfb628485e72324f
egbuniwecemeka <egbuniwecemeka.com>
2024-11-30 13:57:54 +0100

    tests folder & file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('Function', function () {
-    it('should return the sum of two numbers', function () {
+    it('should return the sum of two integers without rounding', function () {
         assert.strictEqual(calculateNumber(2, 3), 5)  // Testing with 2 + 3 = 5
-    })
-})
+    });
+});
