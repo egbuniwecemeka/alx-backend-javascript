56e9de045f7ee683574d666ea361ed2bc9a3ed89
egbuniwecemeka <egbuniwecemeka.com>
2024-12-01 01:17:30 +0100

    Combining descriptions

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,16 +2,20 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('Function', function () {
-  it('If the function type is SUM return its rounded sums', function () {
+  it('If the operator type is SUM return its rounded sums', function () {
     assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
   });
   
-  it('If the function type is SUBTRACT return its difference', function () {
+  it('If the operator type is SUBTRACT return its difference', function () {
     assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   })
 
-  it('if the function type is DIVIDE and divisor is not 0, return remainder', function () {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2)
-  })
+  it('if the operator type is DIVIDE and divisor is not 0, return remainder', function () {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+
+  it('if the divisor is 0, it should return Error', function () {
+    assert.strictEqual(calculateNumber('DIVIDE', 4.5, 0), 'Error');
+  });
 
 });
