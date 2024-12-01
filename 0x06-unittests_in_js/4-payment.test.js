c28a7b1f33978f249b2b605ea79755431fe0ec2b
egbuniwecemeka <egbuniwecemeka.com>
2024-12-01 04:36:54 +0100

    Spies

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,12 +1,14 @@
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment')
 const Utils = require('./utils');
-const calculateNumber = require('./1-calcul');
 
 describe('sendPaymentRequestToApi - Sinon test', function () {
-    it('', function () {
+    it('should call Utils.calculateNumber with SUM, 100, and 20', function () {
         // Create a spy on Utils.calculateNumber
         const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+        // Create a stub for Utils.calculateNumber
+        const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
         // Call sendPaymentTpApi
         sendPaymentRequestToApi(100, 20);
@@ -14,7 +16,11 @@ describe('sendPaymentRequestToApi - Sinon test', function () {
         // Assert that Utils.calculateNumber was called with the right arguments
         sinon.assert.calledOnceExactlyWith(calculateNumberSpy, 'SUM', 100, 20);
 
+        //Asert that Utils.calculateNumber was called with right arguments
+        sinon.assert.calledOnceWith(calculateNumberStub, 'SUM', 100, 20);
+
         // Restore the original function
         calculateNumberSpy.restore();
+        calculateNumberStub.restore()
     });
 });
