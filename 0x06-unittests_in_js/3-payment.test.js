42155c9c9ac9baf72e369246444a25a1f6681159
egbuniwecemeka <egbuniwecemeka.com>
2024-12-01 03:44:57 +0100

    Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,9 +1,18 @@
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi - Sinon test', function () {
-    it('', function () {
-        const addSpy = sendPaymentRequestToApi(100, 20);
-        sinon.spy(addSpy, 'add')
-    })
-})
+    it('should call Utils.calculateNumber with SUM, 100, 20', function () {
+        // Create a spy on Utils.calculateNumber
+        const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+        // Call sendPaymentRequestToApi
+        sendPaymentRequestToApi(100, 20);
+
+        // Assert Utils.calculateNumber was called once with the right arguments
+        sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 100, 20);
+
+        calculateNumberSpy.restore()
+    });
+});
