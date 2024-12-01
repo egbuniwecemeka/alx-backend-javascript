e1ccef54093eb2b421fc84cbb3b36e57c40f6a05
egbuniwecemeka <egbuniwecemeka.com>
2024-12-01 01:39:26 +0100

    Combining descriptions

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,23 @@
+function calculateNumber(type, a, b) {
+    const opsType = ['SUM', 'SUBTRACT', 'DIVIDE'];
+
+  if (!opsType.includes(type)) {
+    throw new Error('Does not include operation type');
+  }
+
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  if (type === opsType[0]) {
+    return roundedA + roundedB;
+  } else if (type === opsType[1]) {
+    return roundedA - roundedB;
+  } else if (type === opsType[2]) {
+    if (roundedB === 0) {
+        return 'Error';
+    }
+    return roundedA / roundedB;
+  }
+}
+
+module.exports = calculateNumber;
