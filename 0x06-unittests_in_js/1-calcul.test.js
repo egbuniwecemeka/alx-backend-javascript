fb1220dd24823b2ea4594172bd2ebdd8867940f3
egbuniwecemeka <egbuniwecemeka.com>
2024-12-01 01:13:38 +0100

    Combining descriptions

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,11 +4,14 @@ const calculateNumber = require('./1-calcul');
 describe('Function', function () {
   it('If the function type is SUM return its rounded sums', function () {
     assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-    assert.strictEqual(calculateNumber('SUM', 2.7, 3.4), 6);
   });
   
   it('If the function type is SUBTRACT return its difference', function () {
     assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  })
+
+  it('if the function type is DIVIDE and divisor is not 0, return remainder', function () {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2)
   })
 
 });
