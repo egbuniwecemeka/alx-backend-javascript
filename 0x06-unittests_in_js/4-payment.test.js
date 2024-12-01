4e28418db4827f986ba1b5dc14f23010396950df
egbuniwecemeka <egbuniwecemeka.com>
2024-12-01 13:39:42 +0100

    Spies

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,18 +4,14 @@ const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi - Sinon test', function () {
     it('should call Utils.calculateNumber with SUM, 100, and 20, als logging the total', function () {
-        // Create a spy on Utils.calculateNumber
-        const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
         // Create a spy on console.log
         const consoleSpy = sinon.spy(console, 'log');
         // Create a stub for Utils.calculateNumber to always return 10
         const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
-        // Call sendPaymentTpApi
+        // Call sendPaymentToApi
         sendPaymentRequestToApi(100, 20);
 
-        // Assert Utils.calculateNumber was called with the right arguments
-        sinon.assert.calledOnceExactlyWith(calculateNumberSpy, 'SUM', 100, 20);
         // Assert that console.log was called with the right message
         sinon.assert.calledOnceExactlyWith(consoleSpy, 'The total is: 10');
         //Asert that Utils.calculateNumber was called with right arguments
