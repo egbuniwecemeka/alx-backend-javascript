d272033de74af7d57c79591a283174716bfc3fb7
egbuniwecemeka <egbuniwecemeka.com>
2024-12-01 02:00:03 +0100

    Combining descriptions

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,12 @@
 const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
-expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-expect(() => calculateNumber('DIVIDE', 2, 0)).to.throw('Error');
+describe('calculateNumber - Chai Tests', function () {
+    it('sums up two integers when operator type is SUM', function () {
+        const result = calculateNumber('SUM', 1.4, 4.5);
+        expect(result).to.equal(6);
+    });
+
+
+})
+//expect(() => calculateNumber('DIVIDE', 2, 0)).to.throw('Error');
