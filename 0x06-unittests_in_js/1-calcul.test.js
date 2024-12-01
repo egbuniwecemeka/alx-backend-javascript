d54b0df132bb764d02bc3d111514e09bec8200b9
egbuniwecemeka <egbuniwecemeka.com>
2024-12-01 01:07:01 +0100

    Combining descriptions

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -6,6 +6,9 @@ describe('Function', function () {
     assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
     assert.strictEqual(calculateNumber('SUM', 2.7, 3.4), 6);
   });
-
+  
+  it('If the function type is SUBTRACT return its difference', function () {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  })
 
 });
