62c2701c876b43421662ec7afa0ebc9a457bce46
egbuniwecemeka <egbuniwecemeka.com>
2024-12-14 02:13:28 +0100

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,63 +1,63 @@
 class ALXCourse {
-    constructor(name, length, students) {
-      // Attribute type check
-      if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-
-      // Attribute type check
-      if (typeof length !== 'number') {
-        throw new TypeError('Length must be a number');
-      }
-
-      if (!Array.isArray(students)) {
-        throw new TypeError('Students must be an array');
-      }
-
-      // Attribute initialization
-      this._name = name;
-      this._length = length;
-      this._students = students;
+  constructor(name, length, students) {
+    // Attribute type check
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
 
-    // Getter name
-    get name() {
-      return this._name;
+    // Attribute type check
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
     }
 
-    // Setter
-    set name(name) {
-      if (typeof name != 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = name;
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
     }
 
-    // Getter length
-    get length() {
-      return this._length;
-    }
+    // Attribute initialization
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-    // Setter length
-    set length(length) {
-      if (typeof length != 'number') {
-        throw new TypeError('Length must be a number');
-      }
-      this._length = length;
-    }
+  // Getter name
+  get name() {
+    return this._name;
+  }
 
-    // Getter students
-    get students() {
-      return this._students;
+  // Setter
+  set name(name) {
+    if (typeof name != 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = name;
+  }
 
-    // Setter students
-    set students(students) {
-      if (!Array.isArray(students)) {
-        throw new TypeError('Students must be an array');
-      }
-      this._students = students;
+  // Getter length
+  get length() {
+    return this._length;
+  }
+
+  // Setter length
+  set length(length) {
+    if (typeof length != 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = length;
+  }
+
+  // Getter students
+  get students() {
+    return this._students;
+  }
+
+  // Setter students
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = students;
+  }
 }
 
 export default ALXCourse;
