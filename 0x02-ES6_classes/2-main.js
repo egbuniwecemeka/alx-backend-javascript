d027c57fd8db4106f4de8e4d7ffcb27d62f61846
egbuniwecemeka <egbuniwecemeka.com>
2024-12-14 02:42:14 +0100

    A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-main.js
@@ -0,0 +1,17 @@
+import ALXCourse from "./2-hbtn_course";
+
+const course = new ALXCourse('ES6', 1, ['Emeka', 'Chidewu']);
+console.log(course.get_name);
+course.set_name = "Python 101";
+
+try {
+  course.set_name = 12;
+} catch (err) {
+  console.error(err);
+}
+
+try {
+  const course2 = new ALXCourse('ES6', 1, ['Emeka', 'Chidewu']);
+} catch (err) {
+    console.log(err);
+}
