1805a8b4f9da99a5662f128c83dc11bb360b7731
egbuniwecemeka <egbuniwecemeka.com>
2024-12-14 01:24:13 +0100

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,17 @@
 export default class ALXCourse {
     constructor(name, length, students) {
-      
+      // Attribute type check
+      if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+
+      // Attribute type check
+      if (typeof length !== 'number') {
+        throw new TypeError('Length must be a number');
+      }
+
+      if (!Array.isArray(students)) {
+        throw new TypeError('Students must be an array');
+      }
     }
 }
