62c1eddb425af5e2a8914e624d68e09285113063
egbuniwecemeka <egbuniwecemeka.com>
2024-12-14 11:50:07 +0100

    Methods, static methods, computed methods names..... MONEY:

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -42,7 +42,7 @@ class Currency {
   }
 
   displayFullCurrency() {
-    return console.log(`${this.get_code}: ${this.get_name}`);
+    return console.log(`${this.get_code} ${this.get_name}`);
   }
 }
 
