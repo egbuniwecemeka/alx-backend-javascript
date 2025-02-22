ec65aa46640594e8c59e28573b0c4319b380ba31
egbuniwecemeka <egbuniwecemeka.com>
2025-02-22 11:58:09 +0100

    eslint fix

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,29 +1,29 @@
 #!/usr/bin/node
 
 export default function createEmployeesObject(departmentName, employees) {
-    const staff = {
-        [departmentName]: employees,
+  const staff = {
+    [departmentName]: employees,
+  }
+
+  let count = -1;
+  const iterable = {
+    [Symbol.iterator]: function() {
+      return { 
+          next: () => {
+              count++;
+              switch(count) {
+                case 0:
+                  return {[departmentName]: staff[departmentName]};
+                default:
+                  return {key: undefined, done: true};
+              }
+          }
+      };
     }
+  };
 
-    let count = -1;
-    const iterable = {
-        [Symbol.iterator]: function() {
-            return { 
-                next: () => {
-                    count++;
-                    switch(count) {
-                        case 0:
-                            return {[departmentName]: staff[departmentName]};
-                        default:
-                            return {key: undefined, done: true};
-                    }
-                }
-             };
-        }
-    };
-
-    const data = iterable[Symbol.iterator]();
+  const data = iterable[Symbol.iterator]();
 
 
-    return data.next();
+  return data.next();
 }
