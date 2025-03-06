edcab567455dff93b387926ba0027b9422fa4328
egbuniwecemeka <egbuniwecemeka.com>
2025-03-06 23:19:57 +0100

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,9 @@
-import { uploadPhoto, createUser } from './utils';
 
-export function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-    .then((value) => {
-      const {body} = value[0];
-      const {firstName, lastName} = value[1];
-      console.log(`${body} ${firstName} ${lastName}`)
-    }).catch(() => {
-      console.log(Error('Signup system offline'));
-    });
+import { uploadPhoto, createUser } from './utils.js'
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+  .then(([photo, user]) => {
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+}).catch(() => console.log('Signup system offline'))
 }
