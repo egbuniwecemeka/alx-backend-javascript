9d141925fecafbed0cd8153e6cc9ad22a3f25c8d
egbuniwecemeka <egbuniwecemeka.com>
2025-03-08 11:34:14 +0100

    user promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
