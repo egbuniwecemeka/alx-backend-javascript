f2dc45b04374f7872c68acfbe65bb162309c8137
egbuniwecemeka <egbuniwecemeka.com>
2025-03-10 12:01:35 +0100

    Throw error / try catch

--- 0x01-ES6_promise/8-main.js
+++ 0x01-ES6_promise/8-main.js
@@ -2,5 +2,9 @@
 
 import divideFunction from "./8-try";
 
-console.log(divideFunction(10, 2));
-console.log(divideFunction(10, 0));
+try {
+  console.log(divideFunction(10, 2));
+  console.log(divideFunction(10, 0));
+} catch (error) {
+  console.log('Error message:', error.message);  
+}
