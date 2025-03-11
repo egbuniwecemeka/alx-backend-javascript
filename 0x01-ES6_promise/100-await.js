ff357633d63d35a2e8c3d1dffd6df1d858dd9507
egbuniwecemeka <egbuniwecemeka.com>
2025-03-11 17:05:30 +0100

    Await / Async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploader() {
   try {
     const photoResponse = await uploadPhoto();
     const userResponse = await createUser();
-    return { photo: photoResponse, user: userResponse };    
+    return { photo: photoResponse, user: userResponse };
   } catch (error) {
-        return { photo: null, user: null };
-  } 
+    return { photo: null, user: null };
+  }
 }
