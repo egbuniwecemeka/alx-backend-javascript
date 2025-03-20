398c40ffda6d62b97468a7a10403f992193000e7
egbuniwecemeka <egbuniwecemeka.com>
2025-03-20 22:04:39 +0100

    Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,20 +1,20 @@
 import Building from "./5-building.js";
 
 export default class SkyHighBuilding extends Building{
-    constructor(sqft, floors) {
-      if (typeof floors !== 'number') {
-        throw new TypeError('floors must be a number');
-      }
-
-      super(sqft);
-      this._floors = floors;
+  constructor(sqft, floors) {
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
     }
 
-    get floors() {
-        return this._floors;
-    }
+    super(sqft);
+    this._floors = floors;
+  }
 
-    evacuationWarningMessage() {
-      return `Evacuate slowly the ${this._floors} floors`;
-    }
+  get floors() {
+      return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
