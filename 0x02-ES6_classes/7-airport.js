65ac88babf872e2eb45f3465e7433ea4fc7e7c7c
egbuniwecemeka <egbuniwecemeka.com>
2025-03-21 10:32:54 +0100

    Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,18 @@
+export default class Airport {
+    constructor(name, code) {
+      if (typeof name !== 'string' || typeof code !== 'string') {
+        throw new TypeError('name or code must be a string');
+      }
+  
+      this._name = name;
+      this._code = code;
+    }
+
+    get [Symbol.toStringTag]() {
+      return `${this._code}`;
+    }
+
+    toString() {
+      return `[object ${this._code}]`;
+    }
+}
---
+++ 0x02-ES6_classes/7-main.js
@@ -0,0 +1,5 @@
+import Airport from "./7-airport";
+
+const airportNA = new Airport('Nnamdi Azikiwe Airport', 'NA1');
+console.log(airportNA);
+console.log(airportNA.toString());
