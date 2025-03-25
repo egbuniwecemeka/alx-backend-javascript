0790f74281b5a42161e4c080c5b501bc92cc5123
egbuniwecemeka <egbuniwecemeka.com>
2025-03-25 23:39:57 +0100

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,5 +4,5 @@ export default function getListStudents() {
   const obj2 = { id: 2, firstName: 'James', location: 'Columbia' };
   const obj3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
   array.push(obj, obj2, obj3);
-  return array
+  return array;
 }
