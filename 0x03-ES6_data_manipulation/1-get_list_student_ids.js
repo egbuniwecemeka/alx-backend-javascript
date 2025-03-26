483b2218d88fe0a11febd27007022e6210eb2a35
egbuniwecemeka <egbuniwecemeka.com>
2025-03-26 20:44:35 +0100

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,13 @@
+import getListStudents from './0-get_list_students.js';
+
+export default function getListStudentIds(obj) {
+  const array = [];
+
+  if (!Array.isArray(obj)) {
+    return [];
+  } else {
+    return getListStudents().map((iterate) => iterate.id);
+  }
+
+  return array;
+}
---
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -0,0 +1,5 @@
+import getListStudentIds from "./1-get_list_student_ids.js";
+import getListStudents from "./0-get_list_students.js";
+
+console.log(getListStudentIds('hey'));
+console.log(getListStudentIds(getListStudents()));
